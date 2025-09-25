5e1d4d24f2aa89455f7b6068d69c33650cd0511e
Erildo07 <erildonunes12@gmail.com>
2025-09-25 19:33:48 -0300

    add php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+    $dbHost = 'Localhost';
+    $udUsername = 'Nunes';
+    $udPassord = 'portugal';
+    $dbName = 'preenchimento-de-dados';
+
+    $conexao = new mysqli($dbHost,$udUsername,$udPassord,$dbNome)
